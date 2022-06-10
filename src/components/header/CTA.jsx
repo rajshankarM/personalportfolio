7c13c6f14a5b2ffa41fd7ddf069be220a92b7af5
rajshankarM <rajshankar607@gmail.com>
2022-06-10 13:16:11 +0530

    cv link

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import CV from '../../assest/cv.pdf'
+import React from "react";
+import CV from "../../assest/cv.pdf";
 
 const CTA = () => {
   return (
     <div className="cta">
-        <a href="https://drive.google.com/file/d/1IxdJRHPTFgWY4cNN-SE4DzxwBHjAX3nJ/view?usp=sharing" target="_blank" className="btn">Download CV</a>
-        <a href="#contact" className="btn btn-primary">Let's Talk</a>
+      <a
+        href="https://drive.google.com/file/d/1Nae96dBG_tBB6OjsocCNP_dW2eUm45il/view?usp=sharing"
+        target="_blank"
+        className="btn"
+      >
+        Download CV
+      </a>
+      <a href="#contact" className="btn btn-primary">
+        Let's Talk
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
