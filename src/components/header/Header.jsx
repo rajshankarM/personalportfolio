9a9d9c4bc10a6ce5e2f0718bb09a3301ab4ed8d8
rajshankarM <rajshankar607@gmail.com>
2022-06-17 17:58:26 +0530

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import './header.css'
-import CTA from './CTA'
-import ME from '../../assest/me.png'
-import HeaderSocials from './HeaderSocials'
-import FaGithub from './HeaderSocials'
+import React from "react";
+import "./header.css";
+import CTA from "./CTA";
+import ME from "../../assest/me.png";
+import HeaderSocials from "./HeaderSocials";
+import FaGithub from "./HeaderSocials";
 
 const Header = () => {
   return (
@@ -14,16 +14,17 @@ const Header = () => {
         <h5 className="text-light">FullStack Developer (MERN)</h5>
         <CTA />
         <HeaderSocials />
-        
 
         <div className="me">
           <img src={ME} alt="me" />
         </div>
 
-        <a href="#contact" className='scroll__down'>Scroll Downnnnn</a>
+        <a href="#contact" className="scroll__down">
+          Scroll Downnnnn
+        </a>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
